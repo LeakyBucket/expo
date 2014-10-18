cb08ca69dc3b6c90afb5fc3a6e732d26df7001e5
Glen Holcomb <DamnBigMan@gmail.com>
2014-10-18 12:07:35 -0600

    Initial List Item work.

--- lib/expo/blocks/list_item.ex
+++ lib/expo/blocks/list_item.ex
@@ -2,23 +2,23 @@ defmodule Expo.Blocks.ListItem do
   defstruct content: "", marker: "", depth: nil, open: true, multi_line: true
 
   def match(line) do
-    case ~r// |> Regex.named_captures(line) do
-      %{"content" => content} ->
-        %__MODULE__{content: content, marker: marker}
+    case ~r/^(?<depth>#{indent_match}#{marker_match}\s{1,4})#{content_match}/ |> Regex.named_captures(line) do
+      %{"content" => content, "marker" => marker, "depth" => depth} ->
+        %__MODULE__{content: content, marker: marker, depth: depth |> String.length}
       _ ->
         false
     end
   end
 
-  defp indent do
-    "\s{0,3}"
+  defp indent_match do
+    "\\s{0,3}"
   end
 
-  defp marker do
-    "(?<marker>\+|\*|-|(\d+\.|\)))"
+  defp marker_match do
+    "(?<marker>\\+|\\*|-|(\\d+[\\.\\)]))"
   end
 
-  defp content do
-    "(?<content>)"
+  defp content_match do
+    "(?<content>\\S.*)"
   end
 end
--- test/expo/blocks/list_item_test.exs
+++ test/expo/blocks/list_item_test.exs
@@ -1,4 +1,52 @@
 defmodule Expo.Blocks.ListItemTest do
   use ExUnit.Case
   use ExSpec
+
+  describe "match/1" do
+    context "with a valid list item" do
+      it "matches with up to three leading spaces" do
+        line = "   - first"
+
+        assert Expo.Blocks.ListItem.match(line) == %Expo.Blocks.ListItem{content: "first", marker: "-", depth: 5}
+      end
+
+      it "matches an un-ordered list item" do
+        line = "  * first"
+
+        assert Expo.Blocks.ListItem.match(line) == %Expo.Blocks.ListItem{marker: "*", depth: 4, content: "first"}
+      end
+
+      it "matches an ordered list item" do
+        line = "  1) first"
+
+        assert Expo.Blocks.ListItem.match(line) == %Expo.Blocks.ListItem{marker: "1)", depth: 5, content: "first"}
+      end
+
+      it "sets the depth for the item" do
+        line = "  *  first"
+
+        assert Expo.Blocks.ListItem.match(line).depth == 5
+      end
+    end
+
+    context "with an invalid list item" do
+      it "doesn't match with four or more leading spaces" do
+        line = "    - first"
+
+        refute Expo.Blocks.ListItem.match(line)
+      end
+
+      it "doesn't match with more than four spaces between the marker and content" do
+        line = "   *     first"
+
+        refute Expo.Blocks.ListItem.match(line)
+      end
+
+      it "doesn't match invalid ordered list markers" do
+        line = "  1.) bob"
+
+        refute Expo.Blocks.ListItem.match(line)
+      end
+    end
+  end
 end
